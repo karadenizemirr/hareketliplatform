2e57e8804da58ad49ce23f3bc9716df15a34b44b
karadenizemirr <karadenizemirr@gmail.com>
2022-07-24 21:25:19 +0300

    Update - 6

--- resources/views/home/base/header.blade.php
+++ resources/views/home/base/header.blade.php
@@ -71,9 +71,8 @@ end: Topbar -->
                             </li>
                             <li><a href="{{route('projects')}}">Referanslar</a></li>
                             <li><a href="{{route('services')}}">Çözümler</a></li>
-                            <li><a href="{{route('contact')}}">İletişim</a></li>
                             <li><a href="{{route('news')}}">Haberler</a></li>
-                            <li><a href="{{route('contact')}}">İletişiö</a></li>
+                            <li><a href="{{route('contact')}}">İletişim</a></li>
                             <li><a href="{{route('request-education')}}" class="text-warning">Eğitim Başvurusu</a></li>
                             <li><a href="{{route('request-service')}}" class="btn btn-warning text-black-50">Servis Talebi</a></li>
                         </ul>
